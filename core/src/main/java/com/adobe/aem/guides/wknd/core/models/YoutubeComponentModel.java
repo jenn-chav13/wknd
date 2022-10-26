2a03718535c197f886b0117013f6ffb5dc8c1ba6
Mateo Valencia <mateo.valencia@somoglobal.com@AAAAB3NzaC1yc2EAAAADAQABAAABgQDE/KFqgV3BNLQzgrpLCK3I5voxD0bnUUZz8hk5ifbBXQSjuHzFnDmXaJQ8OSM1VtaFFpC1fVVji2+rlyMHTN+5C/IuBc++Y6MyjGoUkaWzXbVZdxe0NXF0mgbAPwWZr1pch1O7wXUsh+d+fMPwOt5IbVOlqzymXaaCMTNDt45W8q6sDXDZUm+pFct5fXi8EZhUAFSwhOB/hicM7row06jSWK5yaOnnT/1gz4MD3ZF+0gn4uaO3YY8ibC3T7g68hce82+bMx4jArfgL1uClm3URTKgQlN4QEcmIhQL6iFD/OkmIqXwk7WKvjSuufMT08SdxcbvEWNzTWIm6hRVwiTc+1LLN8eLUvHeh1u4zt45OMcQL3oS0cmaG+R4kDYWeTtYu54Ail76RN9leZ2kqUq2WMt1f8VLQAQ844UFrbrh5prZbKJSUj+5Q3+PkvaxxWL7d1gAfygZ+2FbsYyO3bSij59jF0cKisbnnFVYfLnlIvGBH40KtgXmylEnAV41a1b0=>
2022-10-26 16:46:40 -0500

    API get youtube video data info

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/YoutubeComponentModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/YoutubeComponentModel.java
@@ -1,17 +1,87 @@
 package com.adobe.aem.guides.wknd.core.models;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @Model(adaptables = SlingHttpServletRequest.class)
 public class YoutubeComponentModel {
 
     @ValueMapValue
+    @Optional
     private String videoId;
+    private String videoTitle;
+    private String videoAuthorName;
+    private Logger logger = LoggerFactory.getLogger(YoutubeComponentModel.class);
 
     public String getVideoId() {
-        return "mi-modelo " + videoId;
+        return videoId;
+    }
+
+    public String getVideoTitle() {
+        return videoTitle;
+    }
+
+    public String getVideoAuthorName() {
+        return videoAuthorName;
+    }
+
+    @PostConstruct
+    public void initalize() {
+        logger.info("Entra a Logger Mateo");
+        getApiValues();
+    }
+
+    public void getApiValues() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        try {
+
+            String url = "https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=VIDEO_ID&format=json";
+            url = url.replaceAll("VIDEO_ID", videoId);
+
+            HttpGet request = new HttpGet(url);
+
+            CloseableHttpResponse response = httpClient.execute(request);
+
+            try {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    // return it as a String
+                    String result = EntityUtils.toString(entity);
+                    JsonParser parser = new JsonParser();
+                    JsonElement jsonTree = parser.parse(result);
+                    JsonObject jsonObject = jsonTree.getAsJsonObject();
+                    videoTitle = jsonObject.get("title").toString();
+                    videoAuthorName = jsonObject.get("author_name").toString();
+                }
+
+            
+
+            } catch(Exception error) {
+                logger.error("Entra al error 1", error);
+            } finally {
+                response.close();
+            }
+        } catch(Exception error) {
+            logger.error("Entra al error 2", error);
+        }
     }
     
 }
--- ui.apps/src/main/content/jcr_root/apps/wknd/components/youtubeComponent/youtubecomponent.js
+++ ui.apps/src/main/content/jcr_root/apps/wknd/components/youtubeComponent/youtubecomponent.js
@@ -1,11 +1,14 @@
-
 use(function() {
-
     let youtubeVideoObject = {};
 
     youtubeVideoObject.ytVideo = properties["videoId"];
 
-    youtubeVideoObject.anotherProperty = "currentPage Path:" + currentPage.getPath();
+    fetch(`https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=${youtubeVideoObject.ytVideo}&format=json`)           //api for the get request
+        .then(response => response.json())
+        .then(data => {
+            youtubeVideoObject.ytTitle = data.title;
+            youtubeVideoObject.ytAuthorName = data.author_name;
+        });
 
     return youtubeVideoObject;
 
